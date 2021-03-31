19be2f3b997107838b3acda92b5c7a0aa7d697d2
Alex Pilugin <alex.pilugin@microsec.co.uk>
2021-03-31 01:09:21 +0100

    overlapping transparent images with absolute positioning

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My test of different modes of rendering vector graphics with Apple SwiftUI.
 
 ## Screenshot
 
-![screenshot.png](https://github.com/alexpilugin/ap-swiftui-test-pdf-image/blob/main/Screenshot.png "screenshot.png") 
+![screenshot.png](https://github.com/alexpilugin/ap-swiftui-test-pdf-image/blob/main/Screenshot2.png "screenshot.png") 
--- ap-test-pdf-images-swiftui/ContentView.swift
+++ ap-test-pdf-images-swiftui/ContentView.swift
@@ -3,13 +3,14 @@
 //  ap-test-pdf-images-swiftui
 //
 //  Created by Alex Pilugins on 31/03/2021.
+//  Based on the question: https://stackoverflow.com/questions/61152480/how-can-i-set-an-image-tint-in-swiftui
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 15) {
+        VStack(spacing: 5) {
             
             Image("Circle")
                 .resizable()
@@ -20,24 +21,38 @@ struct ContentView: View {
             Image("Circle")
                 .resizable()
                 .scaledToFit()
-                .foregroundColor(.red)
+                .foregroundColor(.red)           // ignored
                 .background(Color.gray)
                 .frame(width: 100, height: 100)
                 .border(Color.blue, width: 1)
             
             Image("Circle")
-                .renderingMode(.template) // Black Circle
+                .renderingMode(.template)        // Black Circle
                 .colorMultiply(Color.red)
                 .frame(width: 100, height: 100)
                 .border(Color.green, width: 1)
             
             Image("Circle")
-                .renderingMode(.template) // Rendering Mode
-                .foregroundColor(.red) // Red Circle
+                .renderingMode(.template)        // Rendering Mode
+                .foregroundColor(.red)           // Red Circle
                 .frame(width: 100, height: 100)
                 .border(Color.green, width: 1)
+            
+            ZStack(alignment: .top) {
+                Image("Circle")
+                    .renderingMode(.template)     // Rendering Mode
+                    .foregroundColor(Color.green) // Green Circle
+                    .opacity(0.5)
+                    .offset(x: -20, y: 0)
+                Image("Circle")
+                    .renderingMode(.template)      // Rendering Mode
+                    .foregroundColor(Color.yellow) // Green Circle
+                    .opacity(0.5)
+                    //.position(x: 0, y: 0)
+                    .offset(x: 20, y: 0)
+            }
 
-           Spacer()
+
         }.padding()
         .background(Color("WhiteSmoke"))
     }
